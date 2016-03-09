bd20d4d455b5f66bf090fe98a8f9db0c8976ee90
John Park <jwpark417@gmail.com>
2016-03-09 10:56:46 -0500

    Creating Room controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+
   root 'pages#home'
 
   devise_for 	:users,
@@ -10,5 +11,6 @@ Rails.application.routes.draw do
                               }
 
   resources :users, only: [:show]
+  resources :rooms
 
 end
